44c6800debd9ded67d58e84352707a0fff5b5476
Dylan <63183546+diya-he@users.noreply.github.com>
2023-12-02 21:34:22 +0800

    Another version

---
+++ src/main/java/PoolGame/ToStart.java
@@ -0,0 +1,14 @@
+package PoolGame;
+
+
+import javafx.application.Application;
+
+public class ToStart {
+    /**
+     * The entry point of the program
+     * @param args CLI arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
